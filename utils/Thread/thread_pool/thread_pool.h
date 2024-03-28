6870f8c6b083537fb818194863ca82fe293cd2c4
Mikayel Kenjetsyan <michaelkenjetsyan@gmail.com>
2024-03-28 15:27:19 +0400

    Fixed thread_pool.h

--- utils/Thread/thread_pool/thread_pool.h
+++ utils/Thread/thread_pool/thread_pool.h
@@ -6,73 +6,108 @@
 #include <condition_variable>
 #include <functional>
 #include <future>
+#include <atomic>
 #include "../blocking_queue/blocking_queue.h"
 
-class ThreadPool {
+/*
+ A ThreadPool class manages a pool of threads for executing tasks asynchronously.
+ It provides a way to enqueue tasks and get futures representing their results.
+ The ThreadPool ensures efficient utilization of resources by reusing threads and
+ managing a task queue.
+*/
+
+class ThreadPool
+{
 public:
-    ThreadPool(std::size_t num_threads = std::thread::hardware_concurrency(), size_t queue_capacity = 10)
-            : stop(false), task_queue(queue_capacity) {
+    explicit ThreadPool(std::size_t num_threads = std::thread::hardware_concurrency(), size_t queue_capacity = 10)
+            : stop_flag(false), task_queue(queue_capacity)
+    {
 
         num_threads = (num_threads == 0) ? 2 : num_threads;
 
-        for (size_t i = 0; i < num_threads; ++i) {
+        /// Create worker threads and assign tasks to them.
+        for (size_t i = 0; i < num_threads; ++i)
+        {
             workers.emplace_back([this] {
-                while (true) {
+                /// Each worker thread runs in a loop, waiting for tasks to execute.
+                while (true)
+                {
                     std::function<void()> task;
                     {
                         std::unique_lock<std::mutex> lock(queue_mutex);
-                        condition.wait(lock, [this] { return stop || !task_queue.empty(); });
+                        /// Wait until there's a task in the queue or the stop flag is set.
+                        condition.wait(lock, [this] { return stop_flag.test() || !task_queue.empty(); });
 
-                        if (stop && task_queue.empty()) {
+                        if (stop_flag.test() && task_queue.empty())
                             return;
-                        }
 
                         task = std::move(task_queue.pop());
                     }
 
+                    /// Execute the task.
                     task();
                 }
             });
         }
     }
 
+    /// Function to enqueue a task into the ThreadPool.
     template <class F, class... Args>
-    auto enqueue(F&& f, Args&&... args) -> std::future<decltype(f(args...))> {
+    auto enqueue(F&& f, Args&&... args) -> std::future<decltype(f(args...))>
+    {
         using return_type = decltype(f(args...));
-        auto task = std::make_shared<std::packaged_task<return_type()>>(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+        auto task = std::make_shared<std::packaged_task<return_type()>>([=]() mutable {
+            return f(args...);
+        });
 
         {
             std::unique_lock<std::mutex> lock(queue_mutex);
 
-            if (stop) {
+            /// Check if the ThreadPool is stopped.
+            if (stop_flag.test())
                 throw std::runtime_error("ThreadPool is stopped. Cannot enqueue more tasks.");
-            }
 
+            /// Enqueue the task into the task queue.
             task_queue.push([task]() { (*task)(); });
         }
 
+        /// Notify one thread to start executing the task.
         condition.notify_one();
         return task->get_future();
     }
 
-    ~ThreadPool() {
+    /// Waits for all threads to finish executing and cleans up resources.
+    ~ThreadPool()
+    {
         {
             std::unique_lock<std::mutex> lock(queue_mutex);
-            stop = true;
+            /// Set the stop flag to signal threads to exit.
+            stop_flag.test_and_set();
         }
 
+        /// Notify all threads to wake up and check the stop flag.
         condition.notify_all();
 
-        for (std::thread &worker : workers) {
+        /// Join all worker threads.
+        for (std::thread &worker : workers)
+        {
             worker.join();
         }
     }
 
 private:
+    /// Vector to store worker threads.
     std::vector<std::thread> workers;
+
+    /// Task queue for storing tasks.
     LimitedBlockingQueue<std::function<void()>> task_queue;
 
+    /// Mutex for protecting access to the task queue.
     std::mutex queue_mutex;
+
+    /// Condition variable for synchronizing thread activities.
     std::condition_variable condition;
-    bool stop;
+
+    /// Atomic flag to signal threads to stop.
+    std::atomic_flag stop_flag;
 };
